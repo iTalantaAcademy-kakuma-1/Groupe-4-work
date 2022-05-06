c7850033943075ebe40f9359bd87851d18edb88e
Kambale Kibeho <dessaikibeho@gmail.com>
2022-05-06 12:54:17 +0300

    dessai

--- groupe-4-pratictice/src/App.js
+++ groupe-4-pratictice/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DessaiReact from "./DessaiKibeho/DessaiReact"
 
 function App() {
   return (
     <div className="App">
+      <DessaiReact/>
       
     </div>
   );
---
+++ groupe-4-pratictice/src/DessaiKibeho/DessaiReact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function DessaiReact() {
+  return (
+    <div > <p id='titre'>My name is Jovany George and everyone call me George</p>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/XqrZo7zU-oc" 
+Title="YouTube video player" Frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+
+</iframe>
+    </div>
+  )
+}
