0cd5df9ce45c659c85afe264ab99cefda240e1d1
Elie237 <eliemigisha5@gmail.com>
2022-05-06 13:53:08 +0300

    :100:

--- groupe-4-pratictice/src/App.js
+++ groupe-4-pratictice/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import DessaiReact from "./DessaiKibeho/DessaiReact"
 
+import  BavReactBio from "./BavReactBio/BavReactBio"
+
 function App() {
   return (
     <div className="App">
       <DessaiReact/>
+      <BavReactBio/>
       
     </div>
   );
---
+++ groupe-4-pratictice/src/BavReactBio/BavReactBio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Bavon.css'
+
+
+function BavReactBio() {
+  return (
+    <div>
+        <h1 id="ba">This Is What We Do On daily basis</h1>
+        <img src={require('./elie.jpg')} />;
+    </div>
+    
+  )
+}
+
+export default BavReactBio
